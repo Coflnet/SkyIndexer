38340dd1cd9ae25653338cb8d304c19fa3e329e1
Äkwav <xekwavx@gmail.com>
2021-10-15 16:14:22 +0200

    add username update endpoint

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using hypixel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Coflnet.Sky.Indexer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerController : ControllerBase
+    {
+        private readonly ILogger<PlayerController> _logger;
+
+        public PlayerController(ILogger<PlayerController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [Route("{uuid}")]
+        [HttpPatch]
+        public Task<Player> UpdatName(string uuid, string name = null)
+        {
+            return PlayerService.Instance.UpdatePlayerName(uuid);
+        }
+    }
+}

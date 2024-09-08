11d2902afdd3687a12f48fb005b5a0f302b3ef2d
Äkwav <ekwav@coflnet.com>
2024-09-08 14:10:33 +0200

    log whipe checking

--- Services/WhipedTracker.cs
+++ Services/WhipedTracker.cs
@@ -77,6 +77,7 @@ public class WhipedTracker
             return false;
         }
         var request = new RestRequest("api/profile/" + playerUuid + "/hypixel?maxAge=" + DateTimeOffset.UtcNow.AddHours(-1).ToUniversalTime());
+        logger.LogInformation("Checking profile " + profileId + " for player " + playerUuid);
         var response = await profileClient.GetAsync(request);
         var profile = JsonConvert.DeserializeObject<ProfileResponse>(response.Content);
         if (profile == null || profile.stats.SkyBlock.Profiles.ContainsKey(Guid.Parse(profileId).ToString("n")))
